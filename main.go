ea0a441e08cff114e1002cd54945c58278c3ffe2
Rich Wareham <rich@richwareham.com>
2023-11-26 20:53:12 +0000

    add tcell

--- main.go
+++ main.go
@@ -1,7 +1,48 @@
 package main
 
-import "fmt"
+import "log"
+import "os"
+import "github.com/gdamore/tcell/v2"
 
 func main() {
-	fmt.Println("Hello, world.")
+	s, err := tcell.NewScreen()
+	if err != nil {
+		log.Fatalf("%+v", err)
+	}
+	if err := s.Init(); err != nil {
+		log.Fatalf("%+v", err)
+	}
+
+	// Set default text style
+	defStyle := tcell.StyleDefault.Background(tcell.ColorReset).Foreground(tcell.ColorReset)
+	s.SetStyle(defStyle)
+
+	// Clear screen
+	s.Clear()
+
+	s.SetContent(0, 0, 'H', nil, defStyle)
+	s.SetContent(1, 0, 'i', nil, defStyle)
+	s.SetContent(2, 0, '!', nil, defStyle)
+
+	quit := func() {
+		s.Fini()
+		os.Exit(0)
+	}
+	for {
+		// Update screen
+		s.Show()
+
+		// Poll event
+		ev := s.PollEvent()
+
+		// Process event
+		switch ev := ev.(type) {
+		case *tcell.EventResize:
+			s.Sync()
+		case *tcell.EventKey:
+			if ev.Key() == tcell.KeyEscape || ev.Key() == tcell.KeyCtrlC {
+				quit()
+			}
+		}
+	}
 }
